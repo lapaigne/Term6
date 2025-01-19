7441828dd95928130e7a28b287f673c3dba24af0
Oleg Lialin <lyalin.pa1nt3r@gmail.com>
2025-01-19 14:04:50 +0400

    npc nav mesh agent

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using UnityEngine.AI;
 public class NPC : MonoBehaviour
 {
     // Current HP amount
@@ -11,8 +11,36 @@ public class NPC : MonoBehaviour
     // Shared maximum movement speed for each enemy of a specific kind
     public FloatReference MaxSpeed;
     
-    public Vector2 LookDirection {  get; private set; }
+    public Vector2 LookDirection { get; private set; }
     public Vector2 MoveDirection { get; private set; }
+    
+    public Vector2 LookTarget;
+    public Vector2 MoveTarget;
 
     public NPCActions CurrentAction;
+
+    private float _updateTimer = 0;
+    private NavMeshAgent _agent;
+
+    // test only
+    public GameObject Player;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        if (_updateTimer > .5f)
+        {
+            // seek new target
+            _agent.SetDestination(Player.transform.position);
+        }
+        _updateTimer += Time.deltaTime;
+    }
+
+
 }
